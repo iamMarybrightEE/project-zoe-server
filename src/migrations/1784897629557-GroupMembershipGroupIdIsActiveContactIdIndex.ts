69570e1248563c3d8337d499bb0804f6642eed35
iamMarybrightEE <marybrightetim@gmail.com>
2026-08-10 08:26:43 +0300

    update GroupMembershipGroupIdIsActiveContactIdIndex1784897629557 migration

--- src/migrations/1784897629557-GroupMembershipGroupIdIsActiveContactIdIndex.ts
+++ src/migrations/1784897629557-GroupMembershipGroupIdIsActiveContactIdIndex.ts
@@ -4,17 +4,16 @@ export class GroupMembershipGroupIdIsActiveContactIdIndex1784897629557
   implements MigrationInterface
 {
   name = 'GroupMembershipGroupIdIsActiveContactIdIndex1784897629557';
-  transaction = false;
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      'CREATE INDEX CONCURRENTLY "IDX_70871bc95820363218c334143b" ON "group_membership" ("groupId", "isActive", "contactId")',
+      'CREATE INDEX "IDX_70871bc95820363218c334143b" ON "group_membership" ("groupId", "isActive", "contactId")',
     );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      'DROP INDEX CONCURRENTLY "public"."IDX_70871bc95820363218c334143b"',
+      'DROP INDEX "public"."IDX_70871bc95820363218c334143b"',
     );
   }
 }
